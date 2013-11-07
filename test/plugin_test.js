5bfb2236f650c7636a5235cf7f7e539c69e84e1c
Elan Shanker <elan.shanker@gmail.com>
2013-11-07 10:05:39 -0500

    Add `bare` test

--- test/plugin_test.js
+++ test/plugin_test.js
@@ -19,7 +19,7 @@ describe('Plugin', function() {
 
     plugin.compile(content, 'file.coffee', function(error, data) {
       expect(error).not.to.be.ok;
-      expect(data.data).to.equal(expected)
+      expect(data.data).to.equal(expected);
       done();
     });
   });
@@ -31,7 +31,7 @@ describe('Plugin', function() {
 
     plugin.compile(content, 'file.litcoffee', function(error, data) {
       expect(error).not.to.be.ok;
-      expect(data.data).to.equal(expected)
+      expect(data.data).to.equal(expected);
       done();
     });
   });
@@ -47,6 +47,28 @@ describe('Plugin', function() {
       expect(data.data).to.equal(expected);
       expect(data.map).to.be.a('string');
       done();
+    });
+  });
+
+  it('should support explicit bare setting', function(done) {
+    plugin = new Plugin({plugins:{coffeescript:{bare:false}}});
+
+    var content = 'a = 1';
+    var expected = '(function() {\n  var a;\n\n  a = 1;\n\n}).call(this);\n';
+
+    plugin.compile(content, 'file.coffee', function(error, data) {
+      expect(error).not.to.be.ok;
+      expect(data.data).to.equal(expected);
+
+      plugin = new Plugin({plugins:{coffeescript:{bare:true}}});
+      content = 'a = 1';
+      expected = 'var a;\n\na = 1;\n';
+
+      plugin.compile(content, 'file.coffee', function(error, data) {
+        expect(error).not.to.be.ok;
+        expect(data.data).to.equal(expected);
+        done();
+      });
     });
   });
 });
